8b7f81f761f0530a87e0b6c447d0c4e29790e74a
DhumbiAS07 <asdhumbi@gmail.com>
2026-08-19 22:17:52 +0530

    Add edit expense feature

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 expenses = []
 
 def add_expense():
-    amount=input("Enter expense amount: ")
-    category=input("Enter expense category: ")
+    while True:
+        amount = input("Enter expense amount: ")
 
+        try:
+         amount = float(amount)
+         break
+        except ValueError:
+          print("Please enter a valid number.")
+
+    
+    category=input("Enter expense category: ")
+    date=input("Enter expense date: ")
     expense = {
-       "Amount": amount,
-       "Category": category
+       "amount": amount,
+       "category": category,
+       "date":date
     }
     expenses.append(expense)
 
@@ -15,9 +25,15 @@ def add_expense():
 def view_expenses():
     print("\nYour Expenses:")
 
-    for expense in expenses:
-        print("Amount:", expense["Amount"])
-        print("Category:", expense["Category"])
+    if len(expenses) == 0:
+        print("No expenses found.")
+        return
+
+    for index, expense in enumerate(expenses, start=1):
+        print(index, ".")
+        print("Amount:", expense["amount"])
+        print("Category:", expense["category"])
+        print("Date:", expense["date"])
         print("--------------------")
 
 
@@ -25,9 +41,30 @@ def calculate_total():
     total = 0
 
     for expense in expenses:
-        total = total + float(expense["Amount"])
+        total = total + float(expense["amount"])
 
     print("Total Expenses:", total)
+
+def delete_expense():
+    if len(expenses) == 0:
+        print("There are no expenses to delete.")
+        return
+
+    view_expenses()
+
+    choice = input("Enter the expense number to delete: ")
+
+    try:
+        index = int(choice) - 1
+
+        if index >= 0 and index < len(expenses):
+            deleted_expense = expenses.pop(index)
+            print("Expense deleted:", deleted_expense)
+        else:
+            print("Invalid expense number.")
+
+    except ValueError:
+        print("Please enter a valid number.")
 
 
 while True:
@@ -35,7 +72,8 @@ while True:
     print("1. Add Expense")
     print("2. View Expenses")
     print("3. Calculate Total")
-    print("4. Exit")
+    print("4. Delete Expense")
+    print("5. Exit") 
 
     choice = input("Enter your choice: ")
 
@@ -46,7 +84,9 @@ while True:
     elif choice == "3":
         calculate_total()
     elif choice == "4":
-         print("Goodbye!")
-         break
+        delete_expense()
+    elif choice == "5":     
+        print("Goodbye!")
+        break
     else:
         print("Invalid choice. Please try again.")
